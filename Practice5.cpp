d1385e6375fe6a4939e31f57e5d59fe46e5b2495
AIDANCAUGHEY <>
2022-02-03 01:37:49 +0000

    Practice5 done

---
+++ Practice5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// Put the person struct here
+struct Person{
+  string first;
+  string last;
+  Person();
+  Person(string firstName, string lastName);
+};
+
+
+Person::Person() {
+	// Normally, you do not print in a constructor
+	first = "unknown";
+	last = "unknown";
+}
+
+Person::Person(string firstName, string lastName){
+  first = firstName;
+  last = lastName;
+}
+
+void PrintPerson(Person p) {
+    cout << ": " << p.first << " " << p.last << endl;
+}
+
+int main() {
+	// Create two instances of person here, and assign the names.
+  
+  Person a;
+  Person b("Linus","Van Pelt");
+  cout << "Person a";
+  PrintPerson(a);
+  cout << "Person b";
+  PrintPerson(b);
+
+
+	return 0;
+}
